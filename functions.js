65c1c342bd505f949f9a718484893692f7500e63
Codrin Diaconu <diaconu.codrin@gmail.com>
2015-11-16 18:36:14 +0000

    Added the function that sorts with a function

--- functions.js
+++ functions.js
@@ -1,1 +1,17 @@
 //One line comment
+
+//A function that sorts according to a function
+
+//REMEMBER that if the return is lower than 0 str2 will be moved to the left
+//if the return is larger than 0 str2 will be moved to the right
+
+function longest(str1, str2){
+    console.log('comparator was called with: ', str1, str2);
+    return str2.length - str1.length;
+}
+
+var test = ['hello, how are you?', 'there we go','this is some crazy coding thing'];
+
+test.sort(longest);
+
+//
