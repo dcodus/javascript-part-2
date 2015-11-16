007ac6a2fe57c6b8fa9384fb62f2921d1044ded2
Codrin Diaconu <diaconu.codrin@gmail.com>
2015-11-16 19:15:05 +0000

    Added the square function

--- functions.js
+++ functions.js
@@ -16,3 +16,35 @@ test.sort(longest);
 
 //A function that sorts an array of objects based on name length and the email(alphabetically).
 
+var objects = [
+    {name: "John", email: "b"},
+    {name: "Scooby", email: "a"},
+    {name: "Batman", email: "b"},
+    {name: "Superman", email: "x"}
+];
+
+function sortByLength(a,b){
+    return b.name.length - a.name.length
+}
+
+function sortEmail(a,b){
+    if(b.email.toLowerCase() > a.email.toLowerCase()){
+        return -1;
+    } else if(b.email.toLowerCase() < a.email.toLowerCase()){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+//objects.sort(sortByLength);
+objects.sort(sortEmail);
+
+//A function that is passed to map that squares the numbers in a given array
+
+function square(num){
+    return num * num;
+}
+
+var test = [2,3,4,5];
+var squared = test.map(square);
